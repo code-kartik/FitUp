255300d96f8e8d226a8c159fd3b5edc3178a3b21
code-kartik <kartikarora23082003@gmail.com>
2023-06-22 00:42:35 +0530

    Weather API call

--- app/src/main/java/com/example/fitnessapp/DashboardActivity.kt
+++ app/src/main/java/com/example/fitnessapp/DashboardActivity.kt
@@ -12,16 +12,17 @@ import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import com.example.fitnessapp.databinding.ActivityDashboardBinding
-import com.example.fitnessapp.databinding.ActivityMainBinding
 
 class DashboardActivity : AppCompatActivity(), SensorEventListener {
 
-    var sensorManager: SensorManager? = null
-    var totalSteps: Float = 0.0f
-    var previousTotalSteps: Float = 0.0f
-    private var running = false
-
     private lateinit var binding: ActivityDashboardBinding
+    private var sensorManager: SensorManager? = null
+    private var totalSteps: Float = 0.0f
+    private var previousTotalSteps: Float = 0.0f
+    private var running = false
+    private var stepLength: Float = 0.0f
+    private var distanceTraveled: Float = 0.0f
+    private var caloriesBurned: Float = 0.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +36,7 @@ class DashboardActivity : AppCompatActivity(), SensorEventListener {
         supportActionBar?.hide()
 
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager?
+        calculateStepLength()
     }
 
     override fun onResume() {
@@ -42,11 +44,10 @@ class DashboardActivity : AppCompatActivity(), SensorEventListener {
         running = true
         val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
 
-        if(stepSensor == null){
-            Toast.makeText(this,"No Sensor found",Toast.LENGTH_SHORT).show()
-        }
-        else{
-            sensorManager?.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_UI)
+        if(stepSensor == null) {
+            Toast.makeText(this, "No Sensor found", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
 
@@ -55,26 +56,53 @@ class DashboardActivity : AppCompatActivity(), SensorEventListener {
         sensorManager?.unregisterListener(this)
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (running) {
+            totalSteps = event?.values?.get(0) ?: 0f
+            val currentSteps = (totalSteps - previousTotalSteps).toInt()
+            distanceTraveled += calculateDistance(currentSteps)
+            caloriesBurned += calculateCaloriesBurned(currentSteps)
 
-        if(running){
-            totalSteps = p0!!.values[0]
-            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-            binding.stepsTaken.text = ("$currentSteps steps")
+            binding.stepsTaken.text = "$currentSteps steps"
+            binding.distanceTravelled.text = "%.2f meters".format(distanceTraveled)
+            binding.caloriesBurned.text = "%.2f calories".format(caloriesBurned)
         }
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Empty implementation
     }
 
-    fun resetSteps(){
-        binding.stepsTaken.setOnClickListener{
-            Toast.makeText(this,"Long Press to Reset", Toast.LENGTH_SHORT).show()
+    private fun calculateDistance(steps: Int): Float {
+        return steps * stepLength
+    }
+
+    private fun calculateCaloriesBurned(steps: Int): Float {
+        // Calculate the calories burned based on the number of steps
+        // Replace the calculation logic with your own algorithm
+        val calories = steps * 0.05f // Assuming 1 step burns 0.05 calories
+        binding.caloriesBurned.text = calories.toString()
+        return calories
+    }
+
+    private fun calculateStepLength() {
+        // Calculate the average step length based on user's height or use a default value
+        // Replace the calculation logic with your own algorithm
+        val userHeight = 170 // Replace with user's height in centimeters
+        stepLength = userHeight * 0.415f
+        val distance = (stepLength * totalSteps).toString()// Assuming average step length is 41.5% of user's height
+        binding.distanceTravelled.text = distance
+    }
+
+    private fun resetSteps() {
+        binding.stepsTaken.setOnClickListener {
+            Toast.makeText(this, "Long Press to Reset", Toast.LENGTH_SHORT).show()
         }
         binding.stepsTaken.setOnLongClickListener {
             previousTotalSteps = totalSteps
             binding.stepsTaken.text = 0.toString()
+            binding.distanceTravelled.text = "0 meters"
+            binding.caloriesBurned.text = "0 calories"
 
             saveData()
 
@@ -83,16 +111,18 @@ class DashboardActivity : AppCompatActivity(), SensorEventListener {
     }
 
     private fun saveData() {
-        val sharedPreferences: SharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val sharedPreferences: SharedPreferences =
+            getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor? = sharedPreferences.edit()
         editor?.putFloat("key1", previousTotalSteps)
         editor?.apply()
     }
 
-    private fun loadData(){
-        val sharedPreferences: SharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber: Float = sharedPreferences.getFloat("key1",0f)
-        Log.d("MainActivity","$savedNumber")
+    private fun loadData() {
+        val sharedPreferences: SharedPreferences =
+            getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber: Float = sharedPreferences.getFloat("key1", 0f)
+        Log.d("MainActivity", "$savedNumber")
         previousTotalSteps = savedNumber
     }
 }
